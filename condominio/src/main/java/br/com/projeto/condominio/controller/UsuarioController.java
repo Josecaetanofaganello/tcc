9e6c5683117b90694faba512e7d259d6aa483b81
rodrigo <digoaalbuquerque@gmail.com>
2020-09-21 13:25:30 -0300

    Correções para estrutura do back-end

--- condominio/src/main/java/br/com/projeto/condominio/controller/UsuarioController.java
+++ condominio/src/main/java/br/com/projeto/condominio/controller/UsuarioController.java
@@ -1,8 +1,29 @@
 package br.com.projeto.condominio.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.condominio.model.Usuario;
+import br.com.projeto.condominio.repository.UsuarioRepository;
+
+@RestController()
+@RequestMapping(value ="/usuario")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/buscarUsuario")
+	public @ResponseBody List<Usuario> buscarUsuario(){
+		
+		List<Usuario> usuario = usuarioRepository.buscarUsuario();
+		
+		return usuario;
+	}
 
 }
--- condominio/src/main/java/br/com/projeto/condominio/model/Usuario.java
+++ condominio/src/main/java/br/com/projeto/condominio/model/Usuario.java
@@ -1,20 +1,45 @@
 package br.com.projeto.condominio.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable{ 
 
-@Entity(name = "usuarios")
-@Getter @Setter @NoArgsConstructor
-public class Usuario {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@Column
 	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
