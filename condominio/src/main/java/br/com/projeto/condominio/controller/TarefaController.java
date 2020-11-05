fbb9f37343011a4fda107ea1960385b66feef3ad
rodrigo <digoaalbuquerque@gmail.com>
2020-11-05 01:19:28 -0300

    Correção para controller

--- condominio/src/main/java/br/com/projeto/condominio/controller/TarefaController.java
+++ condominio/src/main/java/br/com/projeto/condominio/controller/TarefaController.java
@@ -3,7 +3,11 @@ package br.com.projeto.condominio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,25 +30,25 @@ public class TarefaController {
 		return tarefas;
 	}
 	
-//	@GetMapping("/consultar/{id}")
-//	  public @ResponseBody Tarefa consultar(@PathVariable Long id) {
-//	    return tarefaServiceImpl.consultar(id);
-//	  }
-//
-//	@PostMapping("/salvar")
-//	public @ResponseBody String salvar(@RequestBody Tarefa tarefa) {
-//		tarefaServiceImpl.salvar(tarefa);
-//		return "Sucesso";
-//	}
-//	
-//	@PostMapping("/atualizar")
-//	public void atualizar(@RequestBody Tarefa tarefa) {
-//		tarefaServiceImpl.atualizar(tarefa);
-//	}
-//	
-//	
-//	@DeleteMapping("/deletar/{id}")
-//	public void deletar(@PathVariable Long id) {
-//		tarefaServiceImpl.deletar(id);
-//	}
+	@GetMapping("/consultar/{id}")
+	  public @ResponseBody Tarefa consultar(@PathVariable Long id) {
+	    return tarefaServiceImpl.consultar(id);
+	  }
+
+	@PostMapping("/salvar")
+	public @ResponseBody String salvar(@RequestBody Tarefa tarefa) {
+		tarefaServiceImpl.salvar(tarefa);
+		return "Sucesso";
+	}
+	
+	@PostMapping("/atualizar")
+	public void atualizar(@RequestBody Tarefa tarefa) {
+		tarefaServiceImpl.atualizar(tarefa);
+	}
+	
+	
+	@DeleteMapping("/deletar/{id}")
+	public void deletar(@PathVariable Long id) {
+		tarefaServiceImpl.deletar(id);
+	}
 }
