09ba39250363081404b9032140850b81e6e7f060
rodrigo <digoaalbuquerque@gmail.com>
2020-11-09 23:56:44 -0300

    Adicionada as anoiações para enquete controller

--- condominio/src/main/java/br/com/projeto/condominio/controller/EnqueteController.java
+++ condominio/src/main/java/br/com/projeto/condominio/controller/EnqueteController.java
@@ -3,16 +3,22 @@ package br.com.projeto.condominio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.condominio.model.Enquete;
 import br.com.projeto.condominio.service.EnqueteService;
 
+@RestController
+@RequestMapping("/enquete")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EnqueteController {
 
 	@Autowired
