1401ef9bb85223f25972e32b0ec9ef60127b0c91
jose.faganello <jose.faganello@inlog.com.br>
2020-11-17 22:16:04 -0300

    alteracoes_front

--- publish/app/modules/home/controllers/HomeCtrl.js
+++ publish/app/modules/home/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-    'use strict';
+    //'use strict';
 
     angular
         .module('app')
@@ -10,8 +10,13 @@
     function HomeCtrl($scope, $http, TodoRepository) {
         $scope.todo = {
             id: 0,
-            nome: '',
-            done: false
+            descricao: '',
+            dataInicial: null,
+            dataFinal: null,
+            dataAtualizacao: null,
+            statusTarefa: false,
+            nomeResponsavel:'',
+          
         }
 
         $scope.todos = [];
@@ -25,6 +30,22 @@
             });
             return count;
         };
+
+        $scope.user = function () {
+            loadUser();
+            return $scope.usuario.nome;
+        }
+
+         function loadUser() {
+            $scope.usuario = {
+                id: localStorage.getItem('id'),
+                nome: localStorage.getItem('nome'),
+                email: localStorage.getItem('email'),
+                tipo: localStorage.getItem('tipo')
+            } ;
+        };
+              
+
 
         $scope.save = function (todo) {
             if (todo.id == 0) {
@@ -44,8 +65,18 @@
         }
 
         $scope.delete = function (todo) {
-            var index = $scope.todos.indexOf(todo)
-            $scope.todos.splice(index, 1);
+            toastr.error("<br /><br /><button type='button' id='confirmationRevertYes' class='btn clear'>Yes</button>", 'Deseja realmente apagar?',
+                {
+                    
+                    allowHtml: true,
+                    onShown: function (toast) {
+                        $("#confirmationRevertYes").click(function () {
+                            var index = $scope.todos.indexOf(todo)
+                            $scope.todos.splice(index, 1);
+                        });
+                    }
+                });
+            
         }
 
         $scope.sync = function () {
@@ -53,17 +84,25 @@
         }
 
         function Save(item) {
-            item.id = $scope.todos.length + 1;
+            //item.id = $scope.todos.length + 1;
+            item.dataInicial = Date.now();
             $scope.todos.push(item);
         }
 
         function New() {
             $scope.todo = {
                 id: 0,
-                text: '',
-                done: false
+                descricao: '',
+                dataInicial: Date.now(),
+                dataFinal: null,
+                dataAtualizacao: null,
+                statusTarefa: false,
+                nomeResponsavel: '',
             }
         }
+
+
+
 
         function Load() {
             if (navigator.onLine) {
@@ -96,7 +135,11 @@
                 .getTodos()
                 .then(
                     function (result) {
-                        $scope.todos = result.data;
+                        for (let i = 0; i < result.data.length; i = i + 1) {
+                            result.data[i].dataInicial = result.data[i].dataInicial.replace(' ', 'T');
+                            result.data[i].dataFinal = result.data[i].dataFinal.replace(' ', 'T');
+                        }
+                     $scope.todos = result.data;
                     },
                     function (error) {
                         toastr.error(error.data, "Falha na requisição");
@@ -108,6 +151,11 @@
                 .sync($scope.todos)
                 .then(
                     function (result) {
+
+                        for (let i = 0; i < $scope.todos.length; i = i + 1) {
+                            $scope.todos[i].dataInicial = $scope.todos[i].dataInicial.replace(' ', 'T');
+                            $scope.todos[i].dataFinal = $scope.todos[i].dataFinal.replace(' ', 'T');
+                        }
                         toastr.info(result.data, "Sincronização completa")
                     },
                     function (error) {
--- publish/app/repositories/TodoRepository.js
+++ publish/app/repositories/TodoRepository.js
@@ -10,10 +10,21 @@
     function TodoRepository($http, $rootScope, $location) {
         return {
             getTodos: function () {
-                return $http.get("/api/todos", { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } });
+                return $http.get("http://localhost:8084/tarefa/pesquisar", { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } })
+                    
+             
+
+
             },
             sync: function (todos) {
-                return $http.post("/api/todos", todos, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } });
+                for (let i = 0; i < todos.length; i = i + 1) {
+
+                    todos[i].dataInicial = todos[i].dataInicial.replace('T', ' ');
+                    todos[i].dataFinal = todos[i].dataFinal.replace('T', ' ');
+                   
+                }
+
+                return $http.post("http://localhost:8084/tarefa/atualizar", todos, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } });
             },
         };
     }
