3fc43858b1415ed581b7a6eda5e519c28725a598
rodrigo <digoaalbuquerque@gmail.com>
2020-11-23 01:02:16 -0300

    Correção unidade

---
+++ publish/app/repositories/UnidadeRepository.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('UnidadeRepository', UnidadeRepository);
+
+    UnidadeRepository.$inject = ['$http', '$rootScope', '$location'];
+
+    function UnidadeRepository($http, $rootScope, $location) {
+        return {
+            getUnidades: function () {
+                return $http.get($rootScope.baseUrl +"/unidade/listar", { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } })
+            },
+            sync: function (todos) {
+                return $http.post($rootScope.baseUrl +"/unidade/atualizar", todos, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } });
+            },
+            delete: function (item) {
+                return $http.delete($rootScope.baseUrl +"/unidade/deletar/" + item.id, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } })
+
+            },
+            getUsuarios: function () {
+                return $http.get($rootScope.baseUrl +"/usuario/pesquisar", { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } })
+            },
+        };
+    }
+})();
