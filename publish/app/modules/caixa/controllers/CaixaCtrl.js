db8609501ca89fef258f824a1cc782932d970184
jose.faganello <jose.faganello@inlog.com.br>
2020-11-24 23:39:48 -0300

    alteracoes_validacao_caixa

--- publish/app/modules/caixa/controllers/CaixaCtrl.js
+++ publish/app/modules/caixa/controllers/CaixaCtrl.js
@@ -9,13 +9,13 @@
     CaixaCtrl.$inject = ['$scope', '$http', 'CaixaRepository'];
 
 
-    app.filter('UnsafeHtml', ["$sce",
-        function ($sce) {
-            return function (val) {
-                return $sce.trustAsHtml(val);
-            };
-        }
-    ]);
+    //app.filter('UnsafeHtml', ["$sce",
+    //    function ($sce) {
+    //        return function (val) {
+    //            return $sce.trustAsHtml(val);
+    //        };
+    //    }
+    //]);
 
     function CaixaCtrl($scope, $http, CaixaRepository) {
         $scope.todo = {
@@ -124,63 +124,63 @@
 
 
 
-        $scope.HTMLclick = function () {
+        //$scope.HTMLclick = function () {
 
-            console.log("starting HTMLclick");
-            var pdf = new jsPDF('p', 'pt', 'letter');
+        //    console.log("starting HTMLclick");
+        //    var pdf = new jsPDF('p', 'pt', 'letter');
 
-            // source can be HTML-formatted string, or a reference
-            // to an actual DOM element from which the text will be scraped.
+        //    // source can be HTML-formatted string, or a reference
+        //    // to an actual DOM element from which the text will be scraped.
 
-            var source = $scope.HtmlData;
+        //    var source = $scope.HtmlData;
 
-            // we support special element handlers. Register them with jQuery-style 
-            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-            // There is no support for any other type of selectors 
-            // (class, of compound) at this time.
+        //    // we support special element handlers. Register them with jQuery-style 
+        //    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+        //    // There is no support for any other type of selectors 
+        //    // (class, of compound) at this time.
 
-            var specialElementHandlers = {
-                // element with id of "bypass" - jQuery style selector
-                '#bypassme': function (element, renderer) {
-                    // true = "handled elsewhere, bypass text extraction"
-                    return true;
-                }
-            };
+        //    var specialElementHandlers = {
+        //        // element with id of "bypass" - jQuery style selector
+        //        '#bypassme': function (element, renderer) {
+        //            // true = "handled elsewhere, bypass text extraction"
+        //            return true;
+        //        }
+        //    };
 
-            var margins = {
-                top: 80,
-                bottom: 60,
-                left: 40,
-                width: 522
-            };
+        //    var margins = {
+        //        top: 80,
+        //        bottom: 60,
+        //        left: 40,
+        //        width: 522
+        //    };
 
-            console.log("Building  HTML" + source);
-            // all coords and widths are in jsPDF instance's declared units
-            // 'inches' in this case
-            pdf.fromHTML(
-                source // HTML string or DOM elem ref.
-                , margins.left // x coord
-                , margins.top // y coord
-                , {
-                    'width': margins.width // max width of content on PDF
-                    ,
-                    'elementHandlers': specialElementHandlers
-                },
-                function (dispose) {
-                    // dispose: object with X, Y of the last line add to the PDF 
-                    //          this allow the insertion of new lines after html
+        //    console.log("Building  HTML" + source);
+        //    // all coords and widths are in jsPDF instance's declared units
+        //    // 'inches' in this case
+        //    pdf.fromHTML(
+        //        source // HTML string or DOM elem ref.
+        //        , margins.left // x coord
+        //        , margins.top // y coord
+        //        , {
+        //            'width': margins.width // max width of content on PDF
+        //            ,
+        //            'elementHandlers': specialElementHandlers
+        //        },
+        //        function (dispose) {
+        //            // dispose: object with X, Y of the last line add to the PDF 
+        //            //          this allow the insertion of new lines after html
 
-                    //Didn't work
-                    //   console.log("Saving HTMLclick");
-                    // pdf.save('Test.pdf');
-                },
-                margins
-            );
+        //            //Didn't work
+        //            //   console.log("Saving HTMLclick");
+        //            // pdf.save('Test.pdf');
+        //        },
+        //        margins
+        //    );
 
-            console.log("after from HTML.");
-            pdf.save('Test.pdf');
+        //    console.log("after from HTML.");
+        //    pdf.save('Test.pdf');
 
-        };
+        //};
 
 
 
@@ -308,9 +308,12 @@
         $scope.save = function (todo,tipe) {
 
 
-            if (todo.id == 0 && todo.descricao != '') {
+            if (todo.id == 0 && todo.descricao != '' && todo.valor> 0) {
+
+
                 if (tipe == 'plus') {
                     todo.tipo = '1';
+
                 }
                 else {
                     todo.tipo = '0';
@@ -323,7 +326,7 @@
                 Sync();
               
             } else {
-                Edit();
+                toastr.warning('Valor Inválido, não digite numeros negativos, utilize a opção despesa!');
             }
             New();
         }
